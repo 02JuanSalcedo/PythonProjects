954ef75bcf2068fc141290a11ebe1b79a8f64f29
Juan Salcedo <02Juan.Salcedo@gulllakecs.org>
2019-04-19 12:58:44 -0400

    sCrEW uP

--- Algebra.Calculator/Algebra Calculator.py
+++ Algebra.Calculator/Algebra Calculator.py
@@ -1,3 +1,22 @@
+# Very experimental many changes in code will take place
+
+# plotting linear graphs
+
 # Imported necessary modules and libraries (FINALLY!!!)
 
-import sympy
+import sympy as sy
+
+import matplotlib.pyplot as plt
+
+# Asking for user input to graph
+
+mx = int(input('Enter mx portion of equation: '))
+b = int(input('Enter b portion of equation: '))
+
+print("y =", mx + b)
+
+x = [100, 200, 300, 400]
+y = [287, 487, 587, 687]
+
+plt.plot(x, y)
+plt.show()
